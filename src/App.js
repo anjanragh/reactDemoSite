497036571c74ec28b26b8b852c304376f7dfe4bc
anjanragh <anjanragh@gmail.com>
2020-06-27 00:30:32 -0400

    Added react hooks into useDropdown and modified initial code to use them.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", { id: "something-important" }, [
-  //     React.createElement("h1", {}, "Adopt Me"),
-  //     React.createElement(Pet, {
-  //         name: "Luna",
-  //         animal: "Dog",
-  //         breed: "Havanese",
-  //     }),
-  //     React.createElement(Pet, {
-  //         name: "Pepper",
-  //         animal: "Bird",
-  //         breed: "Cockatiel",
-  //     }),
-  //     React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-  // ]);
-
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="havanese" />
-      <Pet name="Pepper" animal="bird" breed="cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="mixed" />
+      <SearchParams />
     </div>
   );
 };
